d3e3fcb6aae98c1d7feadff49245b38fc173156a
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-01-04 20:03:07 +0100

    show products

--- controllers/products_controller.php
+++ controllers/products_controller.php
@@ -1,1 +1,10 @@
 <?php
+
+if (isset($_POST["products"])) {
+    header("Access-Control-Allow-Origin: *");
+    $url = "https://jsonplaceholder.typicode.com/posts";
+    $libghiti  = file_get_contents($url);
+    echo $libghiti;
+//    echo "test";
+    exit;
+}
