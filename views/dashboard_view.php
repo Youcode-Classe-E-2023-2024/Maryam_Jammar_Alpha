433e718f2ee66eec2176869a4fe951a9dbd7036d
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-01-05 13:18:17 +0100

    update dashboard

--- views/dashboard_view.php
+++ views/dashboard_view.php
@@ -61,7 +61,7 @@
             <li class="mt-0.5 w-full">
                 <a class=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors" href="index.php?page=logout">
                     <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-                        <i class="relative top-0 text-sm leading-normal text-cyan-500 ni ni-collection"></i>
+                        <i class="relative top-0 text-sm leading-normal text-cyan-500 fa fa-sign-out-alt"></i>
                     </div>
                     <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Logout</span>
                 </a>
