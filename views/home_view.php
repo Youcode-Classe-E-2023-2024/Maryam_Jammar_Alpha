0f109780afec99ab4890a850549d639498a936bd
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-01-08 09:16:59 +0100

    update home

--- views/home_view.php
+++ views/home_view.php
@@ -9,12 +9,12 @@
                     Connectez-vous ou inscrivez-vous pour accéder au votre site.
                 </p>
                 <a href="index.php?page=signin">
-                    <button  class="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Login</button>
+                    <button  class="text-white bg-gradient-to-r from-green-500 to-blue-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Login</button>
 
                 </a>
 
                 <a href="index.php?page=signup">
-                    <button class="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Register</button>
+                    <button class="text-white bg-gradient-to-r from-green-500 to-blue-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Register</button>
 
                 </a>
             </div>
