c66c502b1886cb1e1984f762c2f183c95c7a7c37
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-01-08 09:16:47 +0100

    update logo

--- views/add_view.php
+++ views/add_view.php
@@ -4,9 +4,8 @@
     <div class="h-19">
         <i class="absolute top-0 right-0 p-4 opacity-50 cursor-pointer fas fa-times dark:text-white text-slate-400 xl:hidden" sidenav-close></i>
         <a class="block px-8 py-6 m-0 text-sm whitespace-nowrap dark:text-white text-slate-700" href="index.php?page=dashboard">
-            <img src="<?= PATH ?>assets/img/logo-ct-dark.png" class="inline h-full max-w-full transition-all duration-200 dark:hidden ease-nav-brand max-h-8" alt="main_logo" />
-            <img src="<?= PATH ?>assets/img/logo-ct.png" class="hidden h-full max-w-full transition-all duration-200 dark:inline ease-nav-brand max-h-8" alt="main_logo" />
-            <span class="ml-1 font-semibold transition-all duration-200 ease-nav-brand">XENO</span>
+
+            <span class="ml-1 font-bold text-lg text-green-500  transition-all duration-200 ease-nav-brand">XENO</span>
         </a>
     </div>
 
--- views/dashboard_view.php
+++ views/dashboard_view.php
@@ -5,10 +5,10 @@
 <aside class="fixed inset-y-0 flex-wrap items-center justify-between block w-full p-0 my-4 overflow-y-auto antialiased transition-transform duration-200 -translate-x-full bg-white border-0 shadow-xl dark:shadow-none dark:bg-slate-850 max-w-64 ease-nav-brand z-990 xl:ml-6 rounded-2xl xl:left-0 xl:translate-x-0" aria-expanded="false">
     <div class="h-19">
         <i class="absolute top-0 right-0 p-4 opacity-50 cursor-pointer fas fa-times dark:text-white text-slate-400 xl:hidden" sidenav-close></i>
-        <a class="block px-8 py-6 m-0 text-sm whitespace-nowrap dark:text-white text-slate-700" href="">
-            <img src="./assets/img/logo-ct-dark.png" class="inline h-full max-w-full transition-all duration-200 dark:hidden ease-nav-brand max-h-8" alt="main_logo" />
-            <img src="./assets/img/logo-ct.png" class="hidden h-full max-w-full transition-all duration-200 dark:inline ease-nav-brand max-h-8" alt="main_logo" />
-            <span class="ml-1 font-semibold transition-all duration-200 ease-nav-brand">XENO</span>
+        <a class="text-center block px-8 py-6 m-0 text-sm whitespace-nowrap dark:text-white text-slate-700" href="">
+<!--            <img src="./assets/img/xeno.png" class="inline h-full max-w-full transition-all duration-200 dark:hidden ease-nav-brand max-h-8" alt="main_logo" />-->
+<!--            <img src="./assets/img/logo-ct.png" class="hidden h-full max-w-full transition-all duration-200 dark:inline ease-nav-brand max-h-8" alt="main_logo" />-->
+            <span class="ml-1 font-bold text-lg text-green-500  transition-all duration-200 ease-nav-brand">XENO</span>
         </a>
     </div>
 
@@ -28,7 +28,7 @@
             <li class="mt-0.5 w-full">
                 <a class=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors" href="index.php?page=products">
                     <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-                        <i class="relative top-0 text-sm leading-normal text-orange-500 ni ni-calendar-grid-58"></i>
+                        <i class="relative top-0 text-sm leading-normal text-cyan-500 ni ni-calendar-grid-58"></i>
                     </div>
                     <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Products</span>
                 </a>
@@ -37,7 +37,7 @@
             <li class="mt-0.5 w-full">
                 <a class=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors" href="index.php?page=users">
                     <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-                        <i class="relative top-0 text-sm leading-normal text-orange-500 ni ni-calendar-grid-58"></i>
+                        <i class="relative top-0 text-sm leading-normal text-cyan-500 ni ni-calendar-grid-58"></i>
                     </div>
                     <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Users</span>
                 </a>
