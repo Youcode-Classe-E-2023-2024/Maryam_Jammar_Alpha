9ca05648624b18c07661a1b2ee111e7fd0c4fdb3
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-01-08 01:50:42 +0100

    update products

--- assets/js/products.js
+++ assets/js/products.js
@@ -11,7 +11,9 @@ $.ajax({
         products = JSON.parse(data);
 
         products.forEach((product) => {
-            let description = product.body.substring(0, 80) + "...";
+            let description = product.body.substring(0, 60) + "...";
+            let title = product.title.substring(0, 50) + "...";
+
             productsSection.innerHTML += `<tr>
                                     <td class="p-2 align-middle bg-transparent border-white dark:border-white/40 whitespace-nowrap shadow-transparent">
                                         <div class="flex px-2 py-1">
@@ -19,7 +21,7 @@ $.ajax({
 <!--                                                <img src="<?= PATH ?>assets/img/team-2.jpg" class="inline-flex items-center justify-center mr-4 text-sm text-white transition-all duration-200 ease-in-out h-9 w-9 rounded-xl" alt="user1" />-->
                                             </div>
                                             <div class="flex flex-col justify-center">
-                                                <h6 class="mb-0 text-sm leading-normal dark:text-white">${product.title}</h6>
+                                                <h6 class="mb-0 text-sm leading-normal dark:text-white">${title}</h6>
                                             </div>
                                         </div>
                                     </td>
--- views/products_view.php
+++ views/products_view.php
@@ -3,10 +3,10 @@
 <aside class="fixed inset-y-0 flex-wrap items-center justify-between block w-full p-0 my-4 overflow-y-auto antialiased transition-transform duration-200 -translate-x-full bg-white border-0 shadow-xl dark:shadow-none dark:bg-slate-850 max-w-64 ease-nav-brand z-990 xl:ml-6 rounded-2xl xl:left-0 xl:translate-x-0" aria-expanded="false">
     <div class="h-19">
         <i class="absolute top-0 right-0 p-4 opacity-50 cursor-pointer fas fa-times dark:text-white text-slate-400 xl:hidden" sidenav-close></i>
-        <a class="block px-8 py-6 m-0 text-sm whitespace-nowrap dark:text-white text-slate-700" href="https://demos.creative-tim.com/argon-dashboard-tailwind/pages/dashboard.html" target="_blank">
+        <a class="block px-8 py-6 m-0 text-sm whitespace-nowrap dark:text-white text-slate-700" href="index.php?page=dashboard">
             <img src="<?= PATH ?>assets/img/logo-ct-dark.png" class="inline h-full max-w-full transition-all duration-200 dark:hidden ease-nav-brand max-h-8" alt="main_logo" />
             <img src="<?= PATH ?>assets/img/logo-ct.png" class="hidden h-full max-w-full transition-all duration-200 dark:inline ease-nav-brand max-h-8" alt="main_logo" />
-            <span class="ml-1 font-semibold transition-all duration-200 ease-nav-brand">Argon Dashboard 2</span>
+            <span class="ml-1 font-semibold transition-all duration-200 ease-nav-brand">XENO</span>
         </a>
     </div>
 
@@ -75,20 +75,16 @@
             <nav>
                 <!-- breadcrumb -->
                 <ol class="flex flex-wrap pt-1 mr-12 bg-transparent rounded-lg sm:mr-16">
-                    <li class="text-sm leading-normal">
-                        <a class="text-white opacity-50" href="javascript:;">Pages</a>
-                    </li>
-                    <li class="text-sm pl-2 capitalize leading-normal text-white before:float-left before:pr-2 before:text-white before:content-['/']" aria-current="page">Products</li>
+                    <h6 class="mb-0 font-bold text-white capitalize">Products</h6>
                 </ol>
-                <h6 class="mb-0 font-bold text-white capitalize">Products</h6>
             </nav>
 
             <div class="flex items-center mt-2 grow sm:mt-0 sm:mr-6 md:mr-0 lg:flex lg:basis-auto">
-                <div class="flex items-center md:ml-auto md:pr-4">
+                <div class="flex items-center md:ml-auto md:pr-4 mt-4">
                     <div class="relative flex flex-wrap items-stretch w-full transition-all rounded-lg ease">
-                <span class="text-sm ease leading-5.6 absolute z-50 -ml-px flex h-full items-center whitespace-nowrap rounded-lg rounded-tr-none rounded-br-none border border-r-0 border-transparent bg-transparent py-2 px-2.5 text-center font-normal text-slate-500 transition-all">
-                  <i class="fas fa-search"></i>
-                </span>
+                    <span class="text-sm ease leading-5.6 absolute z-50 -ml-px flex h-full items-center whitespace-nowrap rounded-lg rounded-tr-none rounded-br-none border border-r-0 border-transparent bg-transparent py-2 px-2.5 text-center font-normal text-slate-500 transition-all">
+                      <i class="fas fa-search"></i>
+                    </span>
                         <input type="text" class="pl-9 text-sm focus:shadow-primary-outline ease w-1/100 leading-5.6 relative -ml-px block min-w-0 flex-auto rounded-lg border border-solid border-gray-300 dark:bg-slate-850 dark:text-white bg-white bg-clip-padding py-2 pr-3 text-gray-700 transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none focus:transition-shadow" placeholder="Type here..." />
                     </div>
                 </div>
@@ -102,28 +98,30 @@
                             </div>
                         </a>
                     </li>
-                    <li class="flex items-center px-4">
+
+                    <li class="flex items-center px-4 mt-2">
                         <a href="index.php?page=logout" class="p-0 text-sm text-white transition-all ease-nav-brand">
                             <i fixed-plugin-button-nav class="cursor-pointer fa fa-sign-out"></i>
                             <!-- fixed-plugin-button-nav  -->
                         </a>
                     </li>
 
+
                     <!-- notifications -->
 
-                    <li class="relative flex items-center pr-2">
+                    <li class="relative flex items-center pr-2 mt-2">
                         <p class="hidden transform-dropdown-show"></p>
                         <a href="javascript:;" class="block p-0 text-sm text-white transition-all ease-nav-brand" dropdown-trigger aria-expanded="false">
                             <i class="cursor-pointer fa fa-bell"></i>
                         </a>
 
-                        <ul dropdown-menu class="text-sm transform-dropdown before:font-awesome before:leading-default dark:shadow-dark-xl before:duration-350 before:ease lg:shadow-3xl duration-250 min-w-44 before:sm:right-8 before:text-5.5 pointer-events-none absolute right-0 top-0 z-50 origin-top list-none rounded-lg border-0 border-solid border-transparent dark:bg-slate-850 bg-white bg-clip-padding px-2 py-4 text-left text-slate-500 opacity-0 transition-all before:absolute before:right-2 before:left-auto before:top-0 before:z-50 before:inline-block before:font-normal before:text-white before:antialiased before:transition-all before:content-['\f0d8'] sm:-mr-6 lg:absolute lg:right-0 lg:left-auto lg:mt-2 lg:block lg:cursor-pointer">
+                        <ul dropdown-menu class="text-sm transform-dropdown before:font-awesome before:leading-default before:duration-350 before:ease lg:shadow-3xl duration-250 min-w-44 before:sm:right-8 before:text-5.5 pointer-events-none absolute right-0 top-0 z-50 origin-top list-none rounded-lg border-0 border-solid border-transparent dark:shadow-dark-xl dark:bg-slate-850 bg-white bg-clip-padding px-2 py-4 text-left text-slate-500 opacity-0 transition-all before:absolute before:right-2 before:left-auto before:top-0 before:z-50 before:inline-block before:font-normal before:text-white before:antialiased before:transition-all before:content-['\f0d8'] sm:-mr-6 lg:absolute lg:right-0 lg:left-auto lg:mt-2 lg:block lg:cursor-pointer">
                             <!-- add show class on dropdown open js -->
                             <li class="relative mb-2">
                                 <a class="dark:hover:bg-slate-900 ease py-1.2 clear-both block w-full whitespace-nowrap rounded-lg bg-transparent px-4 duration-300 hover:bg-gray-200 hover:text-slate-700 lg:transition-colors" href="javascript:;">
                                     <div class="flex py-1">
                                         <div class="my-auto">
-                                            <img src="../assets/img/team-2.jpg" class="inline-flex items-center justify-center mr-4 text-sm text-white h-9 w-9 max-w-none rounded-xl" />
+                                            <img src="./assets/img/team-2.jpg" class="inline-flex items-center justify-center mr-4 text-sm text-white h-9 w-9 max-w-none rounded-xl" />
                                         </div>
                                         <div class="flex flex-col justify-center">
                                             <h6 class="mb-1 text-sm font-normal leading-normal dark:text-white"><span class="font-semibold">New message</span> from Laur</h6>
@@ -140,7 +138,7 @@
                                 <a class="dark:hover:bg-slate-900 ease py-1.2 clear-both block w-full whitespace-nowrap rounded-lg px-4 transition-colors duration-300 hover:bg-gray-200 hover:text-slate-700" href="javascript:;">
                                     <div class="flex py-1">
                                         <div class="my-auto">
-                                            <img src="../assets/img/small-logos/logo-spotify.svg" class="inline-flex items-center justify-center mr-4 text-sm text-white bg-gradient-to-tl from-zinc-800 to-zinc-700 dark:bg-gradient-to-tl dark:from-slate-750 dark:to-gray-850 h-9 w-9 max-w-none rounded-xl" />
+                                            <img src="./assets/img/small-logos/logo-spotify.svg" class="inline-flex items-center justify-center mr-4 text-sm text-white bg-gradient-to-tl from-zinc-800 to-zinc-700 dark:bg-gradient-to-tl dark:from-slate-750 dark:to-gray-850 h-9 w-9 max-w-none rounded-xl" />
                                         </div>
                                         <div class="flex flex-col justify-center">
                                             <h6 class="mb-1 text-sm font-normal leading-normal dark:text-white"><span class="font-semibold">New album</span> by Travis Scott</h6>
@@ -191,7 +189,7 @@
     <div class="w-full px-6 py-12 mx-auto">
 
         <!--table-->
-        <div class="flex flex-wrap -mx-3">
+        <div class="w-9/10 mx-auto flex flex-wrap -mx-3">
             <div class="flex-none w-full max-w-full px-3 mx-auto">
                 <div class="relative flex flex-col min-w-0 mb-6 break-words bg-white border-0 border-transparent border-solid shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
                     <div class="p-6 pb-0 mb-0 border-b-0 border-b-solid rounded-t-2xl border-b-transparent">
